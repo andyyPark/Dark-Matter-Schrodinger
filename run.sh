1b0c7277ac2a5d26f507b09af08f00064d3c2c5a
Andy Park <andypark@wsip-174-77-58-153.oc.oc.cox.net>
2019-07-04 21:38:11 -0700

    Merge run.sh to master

--- run.sh
+++ run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-#C=(5 10 15 20 25 30 40)
-#SPREAD=(1 2 3.5 5 7.5 10)
-C=(5 10)
-SPREAD=(1 2)
+C=(5 10 15 20 25 30 40)
+SPREAD=(1 2 3.5 5 7.5 10)
 
 # Check if number of particles is given
-if [[ $1 -eq "" ]]
-	then
-		echo "Number of particles not given"
-		exit 1
+if [[ $1 -eq "" ]]; then
+	echo "Number of particles not given"
+	exit 1
 fi
 
+# Run python script
 for c in ${C[*]}
 do
 	for spread in ${SPREAD[*]}
@@ -20,6 +18,27 @@ do
 	done
 done
 
+# Move all the iamges
+if [[ -d "./images" ]]; then
+	cd ./images 
+
+	if [[ -d "n$1" ]]; then
+		cd ../
+		mv *.png ./images/n$1
+			
+	else
+		mkdir n$1
+		cd ../
+		mv *.png ./images/n$1
+	fi
+
+else
+	mkdir images
+	cd ./images
+	mkdir n$1
+	cd ../
+	mv *.png ./images/n$1
+fi
 
 
 
