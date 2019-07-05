d800c24575f28d7ba6b751dff6718ba58d8b8aeb
Andy Park <andypark@wsip-174-77-58-153.oc.oc.cox.net>
2019-07-04 22:27:49 -0700

    Fix handling numbers

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 C=(5 10 15 20 25 30 40)
 SPREAD=(1 2 3.5 5 7.5 10)
+RE='^[0-9]+$'
 
 # Check if number of particles is given
-if [[ $1 -eq "" ]]; then
+if ! [[ $1 =~ $RE ]]; then
 	echo "Number of particles not given"
 	exit 1
 fi
@@ -39,7 +40,6 @@ else
 	cd ../
 	mv *.png ./images/n$1
 fi
-
 
 
 
