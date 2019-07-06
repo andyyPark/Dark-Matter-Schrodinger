77d825fed2004634acd612fd817b153fa5fbfa09
Andy Park <andypark@wsip-174-77-58-153.oc.oc.cox.net>
2019-07-05 22:05:32 -0700

    Merge para-serial and qsub.sh to master

---
+++ para-serial.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#$ -N DMSE
+#$ -q free*,pub64
+#$ -pe openmp 8-64
+
+./run.sh $1 $2 $3> log$1.txt
--- run.sh
+++ run.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-C=(5 10 15 20 25 30 40)
-SPREAD=(1 2 3.5 5 7.5 10)
+NUMBER='^[0-9]+$'
+DECIAML='^[0-9]+(\.[0-9]+)?$'
 
 # Check if number of particles is given
-if [[ $1 -eq "" ]]; then
+if ! [[ $1 =~ $NUMBER ]]; then
 	echo "Number of particles not given"
 	exit 1
 fi
 
+# Check if c is given
+if ! [[ $2 =~ $DECIAML ]]; then
+	echo "C not given"
+	exit 1
+fi
+
+# Check if spread is given
+if ! [[ $3 =~ $DECIAML ]]; then
+	echo "Initial spread not given"
+	exit 1
+fi
+
 # Run python script
-for c in ${C[*]}
-do
-	for spread in ${SPREAD[*]}
-	do
-		python3 DarkMatter.py $1 $c $spread
-	done
-done
+
+echo "Running $2 and $3"
+python3 DarkMatter.py $1 $2 $3
+
 
 # Move all the iamges
 if [[ -d "./images" ]]; then
@@ -39,7 +48,6 @@ else
 	cd ../
 	mv *.png ./images/n$1
 fi
-
 
 
 
